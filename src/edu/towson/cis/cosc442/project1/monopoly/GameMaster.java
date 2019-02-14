f5d463fd77087f6ac7205a069f55122121ba9751
Andrew Knickman <andrewsknick@gmail.com>
2019-02-13 22:01:37 -0500

    Monopoly commit - Added HTML Javadoc documentation for all classes with JAutodoc

--- src/edu/towson/cis/cosc442/project1/monopoly/GameMaster.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/GameMaster.java
@@ -4,12 +4,26 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class GameMaster.
+ */
 public class GameMaster {
 
+	/** The game master. */
 	private static GameMaster gameMaster;
+	
+	/** The Constant MAX_PLAYER. */
 	static final public int MAX_PLAYER = 8;	
+	
+	/** The data. */
 	private GameMasterData data = new GameMasterData(new ArrayList<Player>(), 0);
 
+	/**
+	 * Instance.
+	 *
+	 * @return the game master
+	 */
 	public static GameMaster instance() {
 		if(gameMaster == null) {
 			gameMaster = new GameMaster();
@@ -17,15 +31,26 @@ public class GameMaster {
 		return gameMaster;
 	}
 
+	/**
+	 * Instantiates a new game master.
+	 */
 	public GameMaster() {
 		data.initAmountOfMoney = 1500;
 		data.dice = new Die[]{new Die(), new Die()};
 	}
 
+    /**
+     * Btn buy house clicked.
+     */
     public void btnBuyHouseClicked() {
         data.gui.showBuyHouseDialog(getCurrentPlayer());
     }
 
+    /**
+     * Btn draw card clicked.
+     *
+     * @return the card
+     */
     public Card btnDrawCardClicked() {
         data.gui.setDrawCardEnabled(false);
         CardCell cell = (CardCell)getCurrentPlayer().getPosition();
@@ -41,6 +66,9 @@ public class GameMaster {
         return card;
     }
 
+    /**
+     * Btn end turn clicked.
+     */
     public void btnEndTurnClicked() {
 		setAllButtonEnabled(false);
 		getCurrentPlayer().getPosition().playAction(null);
@@ -60,6 +88,9 @@ public class GameMaster {
 		}
     }
 
+    /**
+     * Btn get out of jail clicked.
+     */
     public void btnGetOutOfJailClicked() {
 		getCurrentPlayer().getOutOfJail();
 		if(getCurrentPlayer().isBankrupt()) {
@@ -78,6 +109,9 @@ public class GameMaster {
 		}
     }
 
+    /**
+     * Btn purchase property clicked.
+     */
     public void btnPurchasePropertyClicked() {
         Player player = getCurrentPlayer();
 		player.purchase();
@@ -85,6 +119,9 @@ public class GameMaster {
 		updateGUI();
     }
     
+    /**
+     * Btn roll dice clicked.
+     */
     public void btnRollDiceClicked() {
 		int[] rolls = rollDice();
 		if((rolls[0]+rolls[1]) > 0) {
@@ -102,6 +139,9 @@ public class GameMaster {
 		}
     }
 
+    /**
+     * Btn trade clicked.
+     */
     public void btnTradeClicked() {
         TradeDialog dialog = data.gui.openTradeDialog();
         TradeDeal deal = dialog.getTradeDeal();
@@ -114,6 +154,11 @@ public class GameMaster {
         }
     }
 
+    /**
+     * Complete trade.
+     *
+     * @param deal the deal
+     */
     public void completeTrade(TradeDeal deal) {
         Player seller = getPlayer(deal.getPlayerIndex());
         Cell property = data.gameBoard.queryCell(deal.getPropertyName());
@@ -121,51 +166,113 @@ public class GameMaster {
         getCurrentPlayer().buyProperty(property, deal.getAmount());
     }
 
+    /**
+     * Draw CC card.
+     *
+     * @return the card
+     */
     public Card drawCCCard() {
         return data.gameBoard.drawCCCard();
     }
 
+    /**
+     * Draw chance card.
+     *
+     * @return the card
+     */
     public Card drawChanceCard() {
         return data.gameBoard.drawChanceCard();
     }
 
 	
+	/**
+	 * Gets the current player.
+	 *
+	 * @return the current player
+	 */
 	public Player getCurrentPlayer() {
 		return getPlayer(data.turn);
 	}
     
+    /**
+     * Gets the current player index.
+     *
+     * @return the current player index
+     */
     public int getCurrentPlayerIndex() {
         return data.turn;
     }
 
+	/**
+	 * Gets the game board.
+	 *
+	 * @return the game board
+	 */
 	public GameBoard getGameBoard() {
 		return data.gameBoard;
 	}
 
+    /**
+     * Gets the gui.
+     *
+     * @return the gui
+     */
     public MonopolyGUI getGUI() {
         return data.gui;
     }
 
+	/**
+	 * Gets the inits the amount of money.
+	 *
+	 * @return the inits the amount of money
+	 */
 	public int getInitAmountOfMoney() {
 		return data.initAmountOfMoney;
 	}
 	
+	/**
+	 * Gets the number of players.
+	 *
+	 * @return the number of players
+	 */
 	public int getNumberOfPlayers() {
 		return data.players.size();
 	}
 
+    /**
+     * Gets the number of sellers.
+     *
+     * @return the number of sellers
+     */
     public int getNumberOfSellers() {
         return data.players.size() - 1;
     }
 
+	/**
+	 * Gets the player.
+	 *
+	 * @param index the index
+	 * @return the player
+	 */
 	public Player getPlayer(int index) {
 		return (Player)data.players.get(index);
 	}
 	
+	/**
+	 * Gets the player index.
+	 *
+	 * @param player the player
+	 * @return the player index
+	 */
 	public int getPlayerIndex(Player player) {
 		return data.players.indexOf(player);
 	}
 
+    /**
+     * Gets the seller list.
+     *
+     * @return the seller list
+     */
     public ArrayList<Player> getSellerList() {
         ArrayList<Player> sellers = new ArrayList<Player>();
         for (Iterator<Player> iter = data.players.iterator(); iter.hasNext();) {
@@ -175,19 +282,41 @@ public class GameMaster {
         return sellers;
     }
 
+	/**
+	 * Gets the turn.
+	 *
+	 * @return the turn
+	 */
 	public int getTurn() {
 		return data.turn;
 	}
 
+	/**
+	 * Gets the util dice roll.
+	 *
+	 * @return the util dice roll
+	 */
 	public int getUtilDiceRoll() {
 		return this.data.utilDiceRoll;
 	}
 
+	/**
+	 * Move player.
+	 *
+	 * @param playerIndex the player index
+	 * @param diceValue the dice value
+	 */
 	public void movePlayer(int playerIndex, int diceValue) {
 		Player player = (Player)data.players.get(playerIndex);
 		movePlayer(player, diceValue);
 	}
 	
+	/**
+	 * Move player.
+	 *
+	 * @param player the player
+	 * @param diceValue the dice value
+	 */
 	public void movePlayer(Player player, int diceValue) {
 		Cell currentPosition = player.getPosition();
 		int positionIndex = data.gameBoard.queryCellIndex(currentPosition.getName());
@@ -201,6 +330,11 @@ public class GameMaster {
 		updateGUI();
 	}
 
+	/**
+	 * Player moved.
+	 *
+	 * @param player the player
+	 */
 	public void playerMoved(Player player) {
 		Cell cell = player.getPosition();
 		int playerIndex = getPlayerIndex(player);
@@ -218,6 +352,9 @@ public class GameMaster {
         data.gui.setTradeEnabled(data.turn, false);
 	}
 
+	/**
+	 * Reset.
+	 */
 	public void reset() {
 		for(int i = 0; i < getNumberOfPlayers(); i++){
 			Player player = (Player)data.players.get(i);
@@ -227,6 +364,11 @@ public class GameMaster {
 		data.turn = 0;
 	}
 	
+	/**
+	 * Roll dice.
+	 *
+	 * @return the int[]
+	 */
 	public int[] rollDice() {
 		if(data.testMode) {
 			return data.gui.getDiceRoll();
@@ -239,6 +381,11 @@ public class GameMaster {
 		}
 	}
 	
+	/**
+	 * Send to jail.
+	 *
+	 * @param player the player
+	 */
 	public void sendToJail(Player player) {
 	    int oldPosition = data.gameBoard.queryCellIndex(getCurrentPlayer().getPosition().getName());
 		player.setPosition(data.gameBoard.queryCell("Jail"));
@@ -250,6 +397,11 @@ public class GameMaster {
 		        jailIndex);
 	}
     
+	/**
+	 * Sets the all button enabled.
+	 *
+	 * @param enabled the new all button enabled
+	 */
 	private void setAllButtonEnabled(boolean enabled) {
 		data.gui.setRollDiceEnabled(enabled);
 		data.gui.setPurchasePropertyEnabled(enabled);
@@ -260,18 +412,38 @@ public class GameMaster {
         data.gui.setGetOutOfJailEnabled(enabled);
 	}
 
+	/**
+	 * Sets the game board.
+	 *
+	 * @param board the new game board
+	 */
 	public void setGameBoard(GameBoard board) {
 		this.data.gameBoard = board;
 	}
 	
+	/**
+	 * Sets the gui.
+	 *
+	 * @param gui the new gui
+	 */
 	public void setGUI(MonopolyGUI gui) {
 		this.data.gui = gui;
 	}
 
+	/**
+	 * Sets the inits the amount of money.
+	 *
+	 * @param money the new inits the amount of money
+	 */
 	public void setInitAmountOfMoney(int money) {
 		this.data.initAmountOfMoney = money;
 	}
 
+	/**
+	 * Sets the number of players.
+	 *
+	 * @param number the new number of players
+	 */
 	public void setNumberOfPlayers(int number) {
 		data.players.clear();
 		for(int i =0;i<number;i++) {
@@ -281,16 +453,27 @@ public class GameMaster {
 		}
 	}
 
+	/**
+	 * Sets the util dice roll.
+	 *
+	 * @param diceRoll the new util dice roll
+	 */
 	public void setUtilDiceRoll(int diceRoll) {
 		this.data.utilDiceRoll = diceRoll;
 	}
 	
+	/**
+	 * Start game.
+	 */
 	public void startGame() {
 		data.gui.startGame();
 		data.gui.enablePlayerTurn(0);
         data.gui.setTradeEnabled(0, true);
 	}
 
+	/**
+	 * Switch turn.
+	 */
 	public void switchTurn() {
 		data.turn = (data.turn + 1) % getNumberOfPlayers();
 		if(!getCurrentPlayer().isInJail()) {
@@ -303,14 +486,25 @@ public class GameMaster {
 		}
 	}
 	
+	/**
+	 * Update GUI.
+	 */
 	public void updateGUI() {
 		data.gui.update();
 	}
 
+	/**
+	 * Util roll dice.
+	 */
 	public void utilRollDice() {
 		this.data.utilDiceRoll = data.gui.showUtilDiceRoll();
 	}
 
+	/**
+	 * Sets the test mode.
+	 *
+	 * @param b the new test mode
+	 */
 	public void setTestMode(boolean b) {
 		data.testMode = b;
 	}
--- src/edu/towson/cis/cosc442/project1/monopoly/GameMasterData.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/GameMasterData.java
@@ -2,16 +2,42 @@ package edu.towson.cis.cosc442.project1.monopoly;
 
 import java.util.ArrayList;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class GameMasterData.
+ */
 public class GameMasterData {
+	
+	/** The dice. */
 	public Die[] dice;
+	
+	/** The game board. */
 	public GameBoard gameBoard;
+	
+	/** The gui. */
 	public MonopolyGUI gui;
+	
+	/** The init amount of money. */
 	public int initAmountOfMoney;
+	
+	/** The players. */
 	public ArrayList<Player> players;
+	
+	/** The turn. */
 	public int turn;
+	
+	/** The util dice roll. */
 	public int utilDiceRoll;
+	
+	/** The test mode. */
 	public boolean testMode;
 
+	/**
+	 * Instantiates a new game master data.
+	 *
+	 * @param players the players
+	 * @param turn the turn
+	 */
 	public GameMasterData(ArrayList<Player> players, int turn) {
 		this.players = players;
 		this.turn = turn;
